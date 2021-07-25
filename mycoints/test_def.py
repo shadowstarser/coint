06f31e4a49ce5024b8af0d982b701bffa1db7024
shadowstarser <shadowstarser@gmail.com>
2021-07-25 19:26:44 +0300

    ferst commited

--- mycoints/test_def.py
+++ mycoints/test_def.py
@@ -1,4 +1,3 @@
-def func1(name):
-    print(f'hello DUDe{name}')
-
-    func1('TEST')
+def application(env, start_response):
+	start_response('200 OK', [('Content-Type','text/html')])
+	return [b"Hello World"]
